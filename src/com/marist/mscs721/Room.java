8050618bc0e1c87b4979db6a59ad464b76884ca8
Vishal <vishal.koosuri1@marist.edu>
2017-04-27 01:06:43 -0400

    Updated with building and location

--- src/com/marist/mscs721/Room.java
+++ src/com/marist/mscs721/Room.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 public class Room {	
 	
 	private String name;
+	private String building;
+	private String location;
 	private int capacity;
 	private ArrayList<Meeting> meetings;
 	
@@ -13,6 +15,43 @@ public class Room {
 		setName(newName);
 		setCapacity(newCapacity);
 		setMeetings(new ArrayList<Meeting>());
+		setBuilding(building);
+		setLocation(location);
+	}
+
+	public void setLocation(String location) {
+		// TODO Auto-generated method stub
+		this.location = location;
+	}
+
+	public String getLocation() {
+		return this.location;
+	}
+	
+	public void setBuilding(String building) {
+		// TODO Auto-generated method stub
+		this.building = building;
+	}
+	
+	public String getBuilding() {
+		return this.building;
+	}
+	
+	public String addNewMeeting(Meeting newMeeting) {
+		if(checkSchedule(newMeeting)) {
+			getMeetings().add(newMeeting);
+			return "Successfully scheduled meeting!";
+		} else
+			return "Room isn't free";
+	}
+	
+	public boolean checkSchedule(Meeting newMeeting) {
+		ArrayList<Meeting> meetingsList = this.meetings;
+		for(Meeting m : meetingsList) {
+			if((newMeeting.getStartTime() == m.getStartTime())) 
+				return false;
+		}
+		return true;
 	}
 
 	public void addMeeting(Meeting newMeeting) {
